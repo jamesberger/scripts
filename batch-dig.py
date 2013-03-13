1719cb22f61aa9cdfee50397ea25da57d45939af
James Berger <james.m.berger@gmail.com>
2013-03-13 10:54:48 -0500

    now with helpful output

--- batch-dig.py
+++ batch-dig.py
@@ -20,10 +20,30 @@
 
 from subprocess import call
 
+
 with open('batch-dig-input.txt') as infile, open('batch-dig-output.txt', 'w') as outfile:
+
+  # We'll display what the input is so the user knows what to expect in the output file
+  print "\n"
+  print "*************************************************************************************************"
+  print "* Welcome to the Batch Dig Utility!                                                             *"
+  print "*                                                                                               *"
+  print "* (Dig has a batch lookup feature, this is just a Python proof of concept for CLI interaction.) *"
+  print "*                                                                                               *"
+  print "*************************************************************************************************\n\n"
+  print "Your input is as follows: \n"
+
+  for line in infile:
+  
+    print line
+
+  print "\n\n"
+  print "Now running dig on the items in the input file...\n"
 
   for line in infile:
  
     call(['dig', line.strip()], stdout=outfile)
+
+  print "Finished. More work sir?\n"
 
 
