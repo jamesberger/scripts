ce7ec3f325b51555bc23a47170006e44df5561e2
James Berger <james.m.berger@gmail.com>
2013-05-01 11:39:37 -0500

    more updates

--- domain-to-country.sh
+++ domain-to-country.sh
@@ -18,8 +18,12 @@
 # Author:
 # ---------------------------------------------------------
 # Written by James Berger
-# Last updated: April 17th 2013
+# Last updated: May 1st 2013
 #
+
+
+# Below is an example comand that has the functionality we want from this script, just not in batch-friendly format
+# dig +short baidu.com | head -n 1 | xargs whois | grep -i country | head -n 1 | cut -d' ' -f 9
 
 
 # Run clear to keep things tidy in the console
@@ -34,8 +38,20 @@ domain_input_file=$1
 domain_output_file=$2
 
 
-# Here we'll verify that the specified input file is actually
-# a valid text file
+# Create a simple header and show what the input and output files are
+
+echo "--------------------------------------------------------"
+echo "  Domain to Country Lookup Tool, v0.2"
+echo -e  "\n \n"
+echo "  User specified input file: "$1
+echo "  User specified output file: "$2
+echo -e  "\n"
+echo "--------------------------------------------------------"
+echo -e  "\n"
+
+
+
+# Here we'll verify that the specified input file is actually a valid text file
 
 # The command 'file' returns the file type, so we can verify if it's a text
 # file without needed to worry about the file extension.
@@ -43,66 +59,36 @@ file_type=$(file $1)
 
 # Here's our if statement for exiting the script if the input file doesn't
 # appear to be a valid text file
-
+echo "Running input file type check..."
 
 if  file $1 | grep -q "ASCII"   
-  then echo "The specified file is ASCII text." 
+  then echo "File check status: Good. The specified file is ASCII text." 
 else
-  echo "The specified file isn't ASCII text, I can't parse that. The file you provided has the following type: "
+  echo "File check status: Bad. The specified file isn't ASCII text, I can't parse that. The file you provided has the following type: "
   echo $file_type | cut -d' ' -f 2
 fi
 
 echo " "
-echo "Our file type is:"$file_type
 
+# Verify that the output file doesn't already exist
+# if it does, make sure the user is ok with overwriting the contents
+# if it doesn't, create it
 
-# this is an example of case usage from the init script for ntp
-#case $1 in
-#        start)
-#                log_daemon_msg "Starting NTP server" "ntpd"
-#                if [ -z "$UGID" ]; then
-#                        log_failure_msg "user \"$RUNASUSER\" does not exist"
-#                        exit 1
-#                fi
-#                lock_ntpdate
-#                start-stop-daemon --start --quiet --oknodo --pidfile $PIDFILE --startas $DAEMON -- -p $PIDFILE $NTPD_OPTS
-#                status=$?
-#                unlock_ntpdate
-#                log_end_msg $status
-#                ;;
-#        stop)
-#                log_daemon_msg "Stopping NTP server" "ntpd"
-#                start-stop-daemon --stop --quiet --oknodo --pidfile $PIDFILE
-#                log_end_msg $?
-#                rm -f $PIDFILE
-#                ;;
-#        restart|force-reload)
-#                $0 stop && sleep 2 && $0 start
-#                ;;
-#        try-restart)
-#                if $0 status >/dev/null; then
-#                        $0 restart
-#                else
-#                        exit 0
-#                fi
-#                ;;
-#        reload)
-#                exit 3
-#                ;;
-#        status)
-#                status_of_proc $DAEMON "NTP server"
-#                ;;
-#        *)
-#                echo "Usage: $0 {start|stop|restart|try-restart|force-reload|status}"
-#                exit 2
-#                ;;
-#esac
+# pull in our lines from the input file
 
+# Maybe run a regex to make sure it's a valid domain name? Sanitize our inputs
+# vs running the risk of our regex saying a domain name is invalid when it isn't
+# seems to be the choice
 
+# begin loop
 
+# run our command on each line
+# add the result of the command to the output file
+# check to see if we've hit the end of the file
+## if we have, exit with a message indicating we're done and there's output in the output file.
+#### verify that there's content in the output file?
+#### if not, give an error?
+## if we haven't, run another loop.
 
+# end loop
 
-
-
-# Below is an example comand that has the functionality we want from this script, just not in batch-friendly format
-# dig +short baidu.com | head -n 1 | xargs whois | grep -i country | head -n 1 | cut -d' ' -f 9
