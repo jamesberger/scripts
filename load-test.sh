8e26fdf662b52bce2f1fd107049f9eaa74d3ff93
James Berger <james.m.berger@gmail.com>
2013-08-13 17:12:34 -0500

    adding elegance

--- load-test.sh
+++ load-test.sh
@@ -1,5 +1,37 @@
 #!/bin/bash
 
-load=$(uptime | cut -d' ' -f 15 | tr -d ",")
+# --------------------------
+# Simple Load Test Utility
+# --------------------------
+# Written by James Berger
+# Last updated: August 13th 2013
 
-echo $load
+
+# Description:
+# ------------
+# This grabs the five minute load average via the uptime command.
+# It compares it to the max_load variable and if the current load
+# average is higher than the max_load variable, it alerts and 
+# sends you an email.
+
+
+# Setting our variables
+max_load=0.001
+
+
+# We call uptime, and then we use cut with space delimiters to grab
+# the five minute load average, then we use translate (tr) to strip
+# out any spaces and commas so we have a nice float value instead
+# of a string.
+current_load=$(uptime | cut -d' ' -f 15 | tr -d "," | tr -d " ")
+
+
+echo $current_load
+echo $max_load
+
+if [ $(echo "$current_load > $max_load"|bc) -eq 1 ]
+then
+ echo -e "zomg, high load!"
+else
+ echo -e "Load is doing alright."
+fi
