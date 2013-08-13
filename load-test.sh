f33818c9482cd30e5f3f744dabc52077bf7b8f1c
James Berger <james.m.berger@gmail.com>
2013-08-13 17:58:34 -0500

    finishing touches added

--- load-test.sh
+++ load-test.sh
@@ -16,8 +16,13 @@
 
 
 # Setting our variables
+#----------------------
+# (note that the max load variable is set to a very low value by
+#  default, 0.001. This is for testing purposes so you can verify
+#  if the script works or not. If you're actually using it, you
+#  should set it to a more reasonable value, like 1 or 0.75)
 max_load=0.001
-
+email_destination=generic-email-address@somewhere.com
 
 # We call uptime, and then we use cut with space delimiters to grab
 # the five minute load average, then we use translate (tr) to strip
@@ -26,12 +31,18 @@ max_load=0.001
 current_load=$(uptime | cut -d' ' -f 15 | tr -d "," | tr -d " ")
 
 
-echo $current_load
-echo $max_load
-
+# Comparing the current load to the max load
+#--------------------------------------------
+# Now we'll use an if statement to see if the current load exceeds
+# what was set for the max load. Bash doesn't like to evaluate 
+# floating numbers, so we'll pipe it out to bc to do a simple
+# 'greater than?' check. If it passes the check, we email the
+# person specified in our email_destination variable and if not,
+# we simply exit.
 if [ $(echo "$current_load > $max_load"|bc) -eq 1 ]
 then
- echo -e "zomg, high load!"
+echo -e "ALERT.\n This is a message from the Simple Load Test Utility. The current load of $current_load has exceeded the maximum defined load of $max_load on the server $(hostname).\n This alert was created on $(date) (local time on $(hostname))." | mail -s "High load alert on $(hostname)" $email_destination
 else
- echo -e "Load is doing alright."
+  exit 0
 fi
+exit 0
