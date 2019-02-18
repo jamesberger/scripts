3f0764cc8f45d5d51cfeeda0c810ab628880bea7
JBerger <jberger@splunk.com>
2019-02-18 12:23:47 -0600

    updating with a small statement so you can see progress per region

--- check-for-instances.sh
+++ check-for-instances.sh
@@ -13,6 +13,7 @@ regions=(us-east-1 us-east-2 us-west-1 us-west-2 eu-central-1 eu-west-1 eu-west-
 tag='Role'
 
 for element in "${regions[@]}"; do
+  echo "Checking region $element..."
   cat $instance_id_file | while read line
     do
       aws ec2 describe-tags --filters "Name=resource-id,Values=$line" "Name=tag-key,Values=$tag" --region $element | grep Value
