98e4e9867b0b48a7af23904ca2f1895dc48cb57f
JBerger <jberger@splunk.com>
2019-02-20 00:09:09 -0600

    adding a few things like a file to log results

--- check-for-instances.sh
+++ check-for-instances.sh
@@ -1,21 +1,29 @@
 #!/usr/bin/env bash
 
 # Echo commands out as they're being run for debugging purposes, uncomment to use
-# set -x
+#set -x
+
+# Date and time the script starts running
+date=$(date +%Y-%m-%d_%H-%M)
+
+# A file to store the results in
+logfile="instance_check_results_$date"
 
 # A text file with one AWS instance ID per line
-instance_id_file='instance-ids.txt'
+instance_id_file='list-of-instances-to-check.txt'
 
-# An array of the current AWS regions, update as needed
+# Array of current AWS regions
 regions=(us-east-1 us-east-2 us-west-1 us-west-2 eu-central-1 eu-west-1 eu-west-2 eu-west-3 eu-north-1 ap-south-1 ap-northeast-2 ap-southeast-1 ap-southeast-2 ap-northeast-1 ca-central-1 sa-east-1)
 
-# Tag value to check
-tag='Role'
+# What tag to check
+tag='Name'
+
 
 for element in "${regions[@]}"; do
-  echo "Checking region $element..."
+  echo "Checking region $element..." | tee -a $logfile
   cat $instance_id_file | while read line
     do
-      aws ec2 describe-tags --filters "Name=resource-id,Values=$line" "Name=tag-key,Values=$tag" --region $element | grep Value
+      echo "Checking for host $line" | tee -a $logfile
+      aws ec2 describe-tags --filters "Name=resource-id,Values=$line" "Name=tag-key,Values=$tag" --region $element | grep Value | tee -a $logfile
     done
-done
+  done
